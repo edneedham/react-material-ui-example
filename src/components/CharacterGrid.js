6bee82678942d5f6386617ab33500d963a76ccc3
Edward Needham <edwardneedham123@gmail.com>
2020-07-06 15:17:02 +0100

    added material ui to character item

--- src/components/CharacterGrid.js
+++ src/components/CharacterGrid.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Paper } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 
 const CharacterGrid = ({ items, isLoading }) => {
   return <div></div>
--- src/components/CharacterItem.js
+++ src/components/CharacterItem.js
@@ -1,10 +1,42 @@
 import React from 'react';
+import {
+  makeStyles,
+  Card,
+  CardContent,
+  Typography
+} from '@material-ui/core';
 
-function CharacterItem(props) {
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12
+  }
+});
+
+
+
+const CharacterItem = (props) => {
+  const classes = useStyles();
+
   return (
-    <div>
-
-    </div>
+    <Card className={classes.root}>
+      <CardContent>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          {props.name}
+        </Typography>
+        <Typography className={classes.title} color="textSecondary">
+          {props.age}
+        </Typography>
+        <Typography className={classes.title} color="textSecondary">
+          {props.eye_color}
+        </Typography>
+      </CardContent>
+    </Card>
   )
 }
 
