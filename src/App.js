91a1af8626d6b25e33b63cec5d6086668dec995a
Edward Needham <edwardneedham123@gmail.com>
2020-07-06 12:02:34 +0100

    added data fetching with axios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(`https://ghibliapi.herokuapp.com/people`)
+
+      console.log(result.data)
+    }
+    fetchItems();
+
+  }, [])
+
   return (
     <div>
-      <h1>Hello React</h1>
+      <Header />
+      <Footer />
     </div>
   );
 }
